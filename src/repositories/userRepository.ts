57aa0d482edf4c685c0aefe9f88662b1f93de337
Hermes Júnior <hermesj.junior@gmail.com>
2022-09-11 19:06:59 -0300

    SignIn SignUp createds

---
+++ src/repositories/userRepository.ts
@@ -0,0 +1,20 @@
+import { prisma } from "./../config/database.js";
+import { CreateUserData } from "./../services/userService.js";
+
+export async function findById(id: number) {
+  return prisma.user.findUnique({
+    where: { id },
+  });
+}
+
+export async function findUserByEmail(email: string) {
+  return prisma.user.findUnique({
+    where: { email },
+  });
+}
+
+export async function insertUser(user: CreateUserData) {
+  return prisma.user.create({
+    data: user,
+  });
+}
